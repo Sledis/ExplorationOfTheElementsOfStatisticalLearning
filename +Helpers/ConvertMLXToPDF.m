6120979e0b5e2dfe2e3887f167ca1277484a4a21
Sledis <sam_edis1@hotmail.com>
2021-07-29 16:17:12 +0100

    Removing some un-needed stuff.

--- +Helpers/ConvertMLXToPDF.m
+++
@@ -1,6 +0,0 @@
-function ConvertMLXToPDF(fileName)
-[filepath,name,ext] = fileparts(fileName);
-pdfFile = fullfile(filepath,strcat(name,'.pdf'));
-matlab.internal.liveeditor.openAndConvert(fileName,pdfFile);
-end
-
